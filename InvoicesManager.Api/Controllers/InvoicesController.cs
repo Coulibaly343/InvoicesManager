ef6d91fcc1300ff27df5f5cbeab321363955110f
Ami <amadoucoulibaly343@onet.pl>
2020-01-22 23:19:43 +0100

    Controllers => Update , Delete

--- InvoicesManager.Api/Controllers/InvoicesController.cs
+++ InvoicesManager.Api/Controllers/InvoicesController.cs
@@ -1,4 +1,6 @@
 ﻿using InvoicesManager.Core.Invoices.Commands.CreateInvoice;
+using InvoicesManager.Core.Invoices.Commands.DeleteInvoice;
+using InvoicesManager.Core.Invoices.Commands.UpdateInvoice;
 using InvoicesManager.Core.Invoices.Queries.GetAllInvoices;
 using InvoicesManager.Core.Invoices.Queries.GetInvoice;
 using Microsoft.AspNetCore.Mvc;
@@ -65,5 +67,48 @@ namespace InvoicesManager.Api.Controllers
             var invoiceId = await Mediator.Send(command);
             return CreatedAtAction(nameof(Get), new { id = invoiceId }, invoiceId);
         }
+
+        /// <summary>
+        /// Update inovice
+        /// </summary>
+        /// <response code="204">Invoice has been updated successfully.</response>
+        /// <response code="400">Invoice hasn't been updated successfully.</response>
+        /// <response code="404">Requested Invoice not found.</response>
+        /// <response code="500">Update Invoice failed.</response>
+        [HttpPut("{invoiceId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> UpdateInvoice([FromBody] UpdateInvoiceCommand command, int invoiceId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            command.Id = invoiceId;
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Delete invoice by id
+        /// </summary>
+        /// <param name="invoiceId">Id of the invoice to delete.</param>
+        /// <response code="204">Invoice deleted.</response>
+        /// <response code="404">Requested Invoice not found.</response>
+        /// <response code="500">Delete Invoice failed.</response>
+        [HttpDelete("{invoiceId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> DeleteInvoice(int invoiceId)
+        {
+            await Mediator.Send(new DeleteInvoiceCommand { Id = invoiceId });
+            return NoContent();
+        }
+
     }
 }
